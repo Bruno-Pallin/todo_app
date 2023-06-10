97cd0b37d42290abe9525060249fabaeb88dcff8
pallinb <pallinb@hotmail.com>
2023-06-10 11:33:34 -0300

    feat: todo list funcional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
 export default class TodoApp extends React.Component {
+  
+  state = {
+    inputValue: '',
+    todoList: [
+      'TCC',
+      'Curso React',
+      'Comprar caderno'
+    ],
+  }
+
+  handleChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  };
+
+  handleSave(e) {
+    const { inputValue, todoList } = this.state;
+    this.setState({
+      inputValue: '',
+      todoList: [
+        ...todoList,
+        inputValue
+      ]
+    })
+  }
+  
   render() {
+    const {inputValue, todoList} = this.state;
     return (
       <div>
         <h1>TodoApp</h1>
-        <input />
-        <button>Salvar</button>
+        <input 
+          value={inputValue}
+          onChange={(e) => this.handleChange(e)}
+          placeholder='Escreva aqui...'
+        />
+        <button onClick={(e) => this.handleSave(e)}>Salvar</button>
+
+        <ul>
+          {todoList.map((todo, index) => {
+            return <li key={index}>{todo}</li>
+          })}
+        </ul>
       </div>
     );
   }
